9df3a913467e361ed51e9aa67c41928cb1fbd019
spike <azamtoiri@gmail.com>
2023-02-24 23:57:31 +0300

    add `providers` url

--- Pharmacy/urls.py
+++ Pharmacy/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^', include('cashiers.urls')),
+    re_path(r'^', include('providers.urls')),
 ]
