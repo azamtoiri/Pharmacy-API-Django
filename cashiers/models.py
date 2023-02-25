38ebee3d871052c8027a2fb9c85254176d321b84
spike <azamtoiri@gmail.com>
2023-02-25 13:47:06 +0300

    add user

--- cashiers/models.py
+++ cashiers/models.py
@@ -1,13 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class Cashiers(models.Model):
-    first_name = models.CharField(max_length=250)
-    last_name = models.CharField(max_length=250)
-    brith_date = models.DateField()
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=250)
+    first_name = models.CharField(max_length=250, verbose_name='Имя')
+    last_name = models.CharField(max_length=250, verbose_name='Фамилия')
+    brith_date = models.DateField(verbose_name='Дата рождения')
+    phone_number = models.CharField(max_length=20, verbose_name='Номер телефона')
+    address = models.CharField(max_length=250, verbose_name='Адрес')
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.first_name
@@ -16,8 +21,8 @@ class Cashiers(models.Model):
 class Accounts(models.Model):
     accounts_id = models.AutoField(primary_key=True)
     cashiers_id = models.ForeignKey(Cashiers, on_delete=models.CASCADE, related_name='cashiers_id')
-    login = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=250)
+    login = models.CharField(max_length=50, unique=True, verbose_name='Login')
+    password = models.CharField(max_length=250, verbose_name='Пароль')
 
     def __str__(self):
         return self.login
