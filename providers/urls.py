76e2eac7c350ec44f436efb50ac14483b630952f
spike <azamtoiri@gmail.com>
2023-02-25 20:29:28 +0300

    switch to generic list api views

--- providers/urls.py
+++ providers/urls.py
@@ -1,11 +1,17 @@
-from django.urls import re_path
-from providers import views
+from django.urls import path
 
 from providers import views
 
 urlpatterns = [
-    re_path(r'^api/providers$', views.providerApi),
-    re_path(r'^api/providers/([0-9]+)$', views.providerApi),
-    re_path(r'^api/medicines$', views.medicine_api),
-    re_path(r'^api/medicines/([0-9]+)$', views.medicine_api),
+    # region: Providers url
+    path('api/providers/create', views.ProviderCreateView.as_view(), name='providers_create'),
+    path('api/providers/', views.ProviderListView.as_view(), name='providers_list'),
+    path('api/providers/<int:pk>', views.ProviderDetailView.as_view(), name='providers_detail'),
+    # endregion
+
+    # region: Medicines url
+    path('api/medicines/create', views.MedicineCreateView.as_view(), name='medicine_create'),
+    path('api/medicines/', views.MedicineListView.as_view(), name='medicine_list'),
+    path('api/medicines/<int:pk>', views.MedicineDetailView.as_view(), name='medicine_detail'),
+    # endregion
 ]
--- providers/views.py
+++ providers/views.py
@@ -1,73 +1,43 @@
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from django.http.response import JsonResponse
+from rest_framework import generics
+# from rest_framework.permissions import IsAdminUser
 
-from providers.models import Provider, Medicine
-from providers.serializers import ProviderSerializer, MedicineSerializer
-
-
-@csrf_exempt
-def providerApi(request, id=0):
-    if request.method == 'GET':
-        providers = Provider.objects.all()
-        providers_serializer = ProviderSerializer(providers, many=True)
-        return JsonResponse(providers_serializer.data, safe=False)
-
-    elif request.method == 'POST':
-        provider_data = JSONParser().parse(request)
-        providers_serializer = ProviderSerializer(data=provider_data)
-        if providers_serializer.is_valid():
-            providers_serializer.save()
-            return JsonResponse(provider_data)
-        return JsonResponse(providers_serializer.errors)
-
-    elif request.method == 'PUT':
-        provider_data = JSONParser().parse(request)
-        provider = Provider.objects.get(Provider_ID=provider_data['id'])
-        providers_serializer = ProviderSerializer(provider, data=provider_data)
-        if providers_serializer.is_valid():
-            providers_serializer.save()
-            return JsonResponse("Updated Successfully!!", safe=False)
-        return JsonResponse(providers_serializer.errors)
-
-    elif request.method == 'DELETE':
-        try:
-            provider = Provider.objects.get(Provider_ID=id)
-            provider.delete()
-            return JsonResponse("Deleted Successfully!!", safe=False)
-        except Provider.DoesNotExist as ex:
-            return JsonResponse(f"Provider does not exist\nError {ex}", safe=False)
+from providers.models import Medicine, Provider
+from providers.serializers import ProviderSerializer, MedicineSerializer, LocalProviderSerializer, \
+    MedicineCreateSerializer
 
 
-@csrf_exempt
-def medicine_api(request, id=0):
-    if request.method == 'GET':
-        medicines = Medicine.objects.all()
-        medicines_serializer = MedicineSerializer(medicines, many=True)
-        return JsonResponse(medicines_serializer.data, safe=False)
+# region: Providers
+class ProviderCreateView(generics.CreateAPIView):
+    serializer_class = ProviderSerializer
+    # permission_classes = ...
 
-    elif request.method == 'POST':
-        medicine_data = JSONParser().parse(request)
-        medicines_serializer = MedicineSerializer(data=medicine_data)
-        if medicines_serializer.is_valid():
-            medicines_serializer.save()
-            return JsonResponse(medicine_data)
-        return JsonResponse(medicines_serializer.errors)
 
-    elif request.method == 'PUT':
-        medicine_data = JSONParser().parse(request)
-        medicine = Medicine.objects.get(Medicine_ID=medicine_data['id'])
-        medicines_serializer = MedicineSerializer(medicine, data=medicine_data)
-        if medicines_serializer.is_valid():
-            medicines_serializer.save()
-            return JsonResponse("Updated Successfully!!", safe=False)
-        return JsonResponse(medicines_serializer.errors)
+class ProviderListView(generics.ListAPIView):
+    serializer_class = LocalProviderSerializer
+    queryset = Provider.objects.all()
 
-    elif request.method == 'DELETE':
-        try:
-            medicine = Medicine.objects.get(Medicine_ID=id)
-            medicine.delete()
-            return JsonResponse("Deleted Successfully!!", safe=False)
-        except Medicine.DoesNotExist as ex:
-            return JsonResponse(f"Medicine does not exist\nError {ex}", safe=False)
 
+class ProviderDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProviderSerializer
+    queryset = Provider.objects.all()
+
+
+# endregion
+
+
+# region: Medicines
+class MedicineCreateView(generics.CreateAPIView):
+    serializer_class = MedicineCreateSerializer
+    # permission_classes = ...
+
+
+class MedicineListView(generics.ListAPIView):
+    serializer_class = MedicineSerializer
+    queryset = Medicine.objects.all()
+
+
+class MedicineDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MedicineSerializer
+    queryset = Medicine.objects.all()
+
+# endregion"
