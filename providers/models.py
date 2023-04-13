299b7be6a379b98c1d03c06eeaae58032c493bca
spike <azamtoiri@gmail.com>
2023-04-13 17:48:58 +0300

    Changed ordering on 'Providers' and 'Medicine'

--- providers/models.py
+++ providers/models.py
@@ -6,6 +6,9 @@ class Provider(models.Model):
     address = models.CharField(max_length=250, verbose_name='Адрес')
     phone_number = models.CharField(max_length=20, verbose_name='Номер телефона')
 
+    class Meta:
+        ordering = ('id',)
+
     def __str__(self):
         return self.name
 
@@ -13,8 +16,11 @@ class Provider(models.Model):
 class Medicine(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя')
     amount = models.PositiveIntegerField(verbose_name='Количество')
-    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name='provider', verbose_name='Поставщики')
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE, related_name='provider', verbose_name='Поставщик')
     end_date = models.DateField(null=True, blank=True, default=None, verbose_name='Срок годности')
+
+    class Meta:
+        ordering = ('id',)
 
     def __str__(self):
         return self.name
